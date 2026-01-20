c8d6e94ef1c9bcb4a458f9973703dec85dbe3ece
nurqalik <qaliksulistyo@gmail.com>
2026-01-20 18:22:25 +0700

    Update: prisma 6 > prisma 7

--- src/server/db.ts
+++ src/server/db.ts
@@ -5,6 +5,7 @@ const createPrismaClient = () =>
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+      datasourceUrl: process.env.DATABASE_URL,
   });
 
 const globalForPrisma = globalThis as unknown as {
